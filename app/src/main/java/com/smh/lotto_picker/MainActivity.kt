b8b322c613921444f6b5d43516314a4def8a518c
MinhyeokSun <s.minyeok@gmail.com>
2021-12-10 22:01:35 +0900

    5th Commit

--- app/src/main/java/com/smh/lotto_picker/MainActivity.kt
+++ app/src/main/java/com/smh/lotto_picker/MainActivity.kt
@@ -3,26 +3,120 @@ package com.smh.lotto_picker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.NumberPicker
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.isVisible
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private val clearButton: Button by lazy {
+        findViewById<Button>(R.id.clearButton)
+    }
+
+    private val addButton: Button by lazy {
+        findViewById(R.id.addButton)
+    }
+
+    private val runButton: Button by lazy { //
+        findViewById(R.id.runButton)
+    }
+
+    private val numberPicker: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker)
+    }
+    private val numberTextViewList: List<TextView> by lazy {
+        listOf<TextView>(
+            findViewById<TextView>(R.id.firstNum),
+            findViewById<TextView>(R.id.secondNum),
+            findViewById<TextView>(R.id.thirdNum),
+            findViewById<TextView>(R.id.forthNum),
+            findViewById<TextView>(R.id.fifthNum),
+            findViewById<TextView>(R.id.sixthNum)
+        )
+    }
+
+    private var didRun = false
+
+    private val pickNumberSet = hashSetOf<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        randomNum()
+
+        numberPicker.minValue = 1  // 최소 값
+        numberPicker.maxValue = 45 // 최대 값
+
+        initRubButton()
+        initAddButton()
+        initClearButton()
     }
 
-    fun randomNum() {
-
-        val random = Random(45)
-        val list = mutableListOf<Int>().apply {
-            for (i in 1..45) {
-
+    private fun initClearButton() {
+        clearButton.setOnClickListener {
+            pickNumberSet.clear()
+            numberTextViewList.forEach {
+                it.isVisible = false
             }
+            didRun = false
         }
+    }
 
-        list.shuffle()
-        println(list.subList(0, 6))
-        Log.e("print","${list.subList(0,6)}")
+    private fun initRubButton() {
+        runButton.setOnClickListener {
+            val list = getRandomNumber()
+
+            didRun = true
+
+            list.forEachIndexed { index, number ->
+                val textView = numberTextViewList[index]
+
+                textView.text = number.toString()
+                textView.isVisible = true
+            }
+
+        }
+    }
+
+    private fun initAddButton() {
+        addButton.setOnClickListener {
+            if (didRun) {
+                Toast.makeText(this, "초기화 후에 시도해주세요. ", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (pickNumberSet.size >= 5) {
+                Toast.makeText(this, "번호는 5개까지만 선택할 수 있습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (pickNumberSet.contains(numberPicker.value)) {
+                Toast.makeText(this, "이미 선택한 번호입니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val textView = numberTextViewList[pickNumberSet.size]
+            textView.isVisible = true;
+            textView.text = numberPicker.value.toString()
+
+            pickNumberSet.add(numberPicker.value)
+        }
+    }
+
+    private fun getRandomNumber(): List<Int> {
+        val numberList = mutableListOf<Int>()
+            .apply {
+                for (i in 1..45) {
+                    if(pickNumberSet.contains(i)) {
+                        continue
+                    }
+                    this.add(i)
+                }
+            }
+        numberList.shuffle()
+
+        val newList = pickNumberSet.toList() + numberList.subList(0, 6 - pickNumberSet.size)
+        return newList.sorted()
     }
 }
