5c567376b8c0a178863809dddba4adf29fcb3b1c
MinhyeokSun <s.minyeok@gmail.com>
2021-12-10 18:10:55 +0900

    2nd Commit

--- app/src/main/java/com/smh/lotto_picker/MainActivity.kt
+++ app/src/main/java/com/smh/lotto_picker/MainActivity.kt
@@ -2,10 +2,26 @@ package com.smh.lotto_picker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        randomNum()
+    }
+
+    fun randomNum() {
+
+        val random = Random(45)
+        val list = mutableListOf<Int>()
+
+        while(list.size < 6) {
+            val randomNumber = random.nextInt(45) + 1
+            if (list.contains (randomNumber)) {
+                continue
+            }
+            list.add(randomNumber)
+        }
     }
 }
